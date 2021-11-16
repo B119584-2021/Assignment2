1a96f6594098598b671bedd57449444b1f8687de
Sophie Winterbourne <s1767247@bioinfmsc5.bio.ed.ac.uk>
2021-11-16 23:56:02 +0000

    Updated fetch_protein_seq_data.py to contain the first version of commands outlined in pseudocode, additional tests needed

--- fetch_protein_seq_data.py
+++ fetch_protein_seq_data.py
@@ -1,15 +1,41 @@
-### TASK 1 ### 
+### TASK 1: Fetching user specified  protein sequence data ### 
+# The aim of this script is to fetch protein sequence data detailed in user input and to provide the user with a summary of actions taken. 
+
+#!/usr/bin/python3
+
+# Load necessary modules 
+import os, subprocess 
 
 ### ASK USER FOR INFO ###
 
-# Which protein family?
+# Ask user to specify protein family
+protein_family=input("Which protein family are you analysing?\n\t> ")
+print(protein_family, "selected")
 
-# Which taxonomic group?
+# Ask user to specify taxonomic group
+taxonomic_group=input("Which taxonomic group are you analysing?\n\t> ")
+print(taxonomic_group, "selected")
+
+esearch_command="esearch -db protein_family -query taxonomic_group"
 
 
-# These will then be input to the command used to fetch the sequences, 
+	#esearch -db protein -query "taxonomic_group AND protein_family" | efetch -db protein -format uid > uid_output
+
+	#cat uid_output | wc -l > seq_number
+	
+	#if (seq_number > 1000):
+	#	answer=input("More than 1000 sequences, do you wish to continue (Yes or No)?\n\t> ")
+	#	if answer == "Yes":
+	#		print("Continuing to the next step")
+	#	else:
+	# 		 exit("Exiting script due to 1000+ sequences"
+
+# These will then be input to the command used to fetch the sequences,
 # before downloading check the number of sequeces and the composition
 
+
+# Perform command to fetch protein sequence data
+subprocess.call("esearch_command, shell=True")
 
 ### CHECK IN WITH USER ###
 
@@ -18,4 +44,4 @@
 # The sequences come from more than one species - do you wish to continue?
 
 
-# Once sequences are fetched provide user with a summary 
+# Once sequences are fetched provide user with a summary
