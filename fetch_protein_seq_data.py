f8d7aa8fce8a9790e6892089fbe8c5300461b495
Sophie Winterbourne <s1767247@bioinfmsc5.bio.ed.ac.uk>
2021-11-16 16:09:59 +0000

    Wrote pseudocode outlining the steps and commands I likely will need in fetch_protein_seq_data.py

---
+++ fetch_protein_seq_data.py
@@ -0,0 +1,21 @@
+### TASK 1 ### 
+
+### ASK USER FOR INFO ###
+
+# Which protein family?
+
+# Which taxonomic group?
+
+
+# These will then be input to the command used to fetch the sequences, 
+# before downloading check the number of sequeces and the composition
+
+
+### CHECK IN WITH USER ###
+
+# More than 1000 sequences - do you wish to continue?
+
+# The sequences come from more than one species - do you wish to continue?
+
+
+# Once sequences are fetched provide user with a summary 
