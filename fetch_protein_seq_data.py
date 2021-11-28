ca0f097ad024062c2f7ee4ab26bace2fa4046012
Sophie Winterbourne <s1767247@bioinfmsc5.bio.ed.ac.uk>
2021-11-28 22:15:59 +0000

    added error traps to prevent analysis continuing if no sequences found, and expanded on test for 1000> sequences

--- fetch_protein_seq_data.py
+++ fetch_protein_seq_data.py
@@ -15,10 +15,28 @@ import os, subprocess, sys, re
 
 # Ask user to specify protein family
 protein_fam=input("Which protein family are you analysing?\n\t> ")
+
+# If variable is empty, repeat request
+while not protein_fam :
+  print("Please provide a protein family")
+  try :
+   protein_fam=input("\t> ")
+  except :
+    continue
+
+# Print selection of protein_fam to screen
 print(protein_fam, "selected\n")
 
+
 # Ask user to specify taxonomic group
-taxon_gr=input("Which taxonomic group are you analysing?\n\t> ")
+taxon_gr = input("Which taxonomic group are you analysing?\n\t> ")
+# If variable is empty, repeat request
+while not taxon_gr :
+  print("Please provide a taxon group")
+  try :
+   taxon_gr = input("\t> ")
+  except:
+    continue
 print(taxon_gr, "selected\n")
 
 
@@ -30,13 +48,9 @@ full_query = quote + taxon_gr + space + protein_fam + quote
 entrez_sum=" > entrez_sum.txt"
 fetch_fasta=" | efetch -format fasta > fasta_seq.fa"
 
+# Combine variables to generate esearch command variables 
 esearch_count="esearch -db protein -query "+full_query+entrez_sum
 esearch_fasta="esearch -db protein -query "+full_query+fetch_fasta
-
-
-
-# Remove pre-existing file if present
-rm -r entrez_sum.txt
 
 
 
@@ -50,8 +64,12 @@ with open("entrez_sum.txt") as my_file:
 # Format entrez_sum to a list so it can be iterated over
 entrez_summary = entrez_sum.split("\n")
 
-# Find countline 
+
+## Find countline 
+# Create empty list
 count_line = []
+
+# Iterate over summary and store line with sequence count
 for sum in entrez_summary:
      if re.search(r'Count', sum) :
              count_line.append(sum)
@@ -59,38 +77,54 @@ for sum in entrez_summary:
 # Extract count from countline
 count = re.findall(r'\d+',str(count_line))
 count_str = str(count).strip("['']")
+
+# Set sequence count as variable x
 x = int(count_str)
 
+if x < 1 :
+  print("There were no sequences found for your search")
+  sys.exit("Exiting...\n")
+
+# Print sequence count to screen for user to see
 print("Number of sequences:",+x,"\n")
 
 
 
 ### >1000 sequences ###
 
-# This block interacts with the user if there are more than 1000 sequences present
+# This block interacts with the user if > 1000 or < 10
 
 # Find out how user would like to proceed:
-if (x > 1000):
-  while True:
-    try:
-      a=input("More than 1000 sequences, do you wish to continue? (Yes or No)\n\t> ")
-      if a == "Yes" or a == "No":
-        break;
-      else:
-        print("Please type Yes or No\n")
-    except:
-      continue
+while x > 1000 :
+  a = input("More than 1000 sequences, do you wish to continue? (Yes or No)\n\t> ")
+  try :
+    if a == "Yes" or a == "No" :
+      break ;
+    a = input("Please type Yes or No\n\t> ")
+  except :
+    continue
 
-# If user wishes to continue despite large size:
-if (a == "Yes"):
-  print("Continuing...\n")
+if (not a == True) :
+  if (a == "Yes") :
+    print("Continuing...\n")
+  if (a == "No"):
+    sys.exit("Exiting due to more than 1000 sequences\n")
 
-# If user wishes to exit due to large size:
-if (a == "No"):
-  sys.exit("Exiting...\n")
 
-#print to check you made it to the bottom
-print("you made it\n")
+# If there are fewer than 10 sequences
+while x <= 10 :
+  b = input("There are less than 10 sequences, would you like to continue? (Yes or No)\n\t> ")
+  try :
+    if b == "Yes" or b == "No" :
+      break ;
+  except :
+    continue
+
+if (not b == True) :
+  if b == "Yes" :
+    print("Continuing")
+  if b == "No" :
+    sys.exit("Exiting due to less than 10 sequences\n")
 
 
 
@@ -101,6 +135,3 @@ print("Fetching sequences in FASTA format\n")
 subprocess.call(esearch_fasta, shell=True)
 
 
-
-### Remove temporary files ###
-rm -r entrez_sum.txt
